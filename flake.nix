bd93bcf47d8b0d5683446479787d60738b8a91d9
xhos <60789741+xhos@users.noreply.github.com>
2025-11-18 21:18:45 -0500

    add test vm command

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
   };
 
   outputs = {
+    self,
     nixpkgs,
     git-hooks,
     ...
@@ -28,10 +29,54 @@
 
     devShells = forAllSystems (pkgs: {
       default = pkgs.mkShell {
-        packages = with pkgs; [
-          go
-        ];
+        packages = with pkgs; [ go ];
       };
     });
+
+    packages = forAllSystems (pkgs: {
+      yawn = pkgs.buildGoModule {
+        pname = "yawn";
+        version = "0.1.0";
+        src = ./.;
+        vendorHash = "sha256-RNbS40G+8rtwlSJgYLN1puTCytGfXdagQTEs6sIXwnM=";
+        ldflags = [ "-s" "-w" ];
+        subPackages = [ "cmd/yawn" ];
+      };
+
+      default = self.packages.${pkgs.system}.yawn;
+    });
+
+    nixosConfigurations.test-vm = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ({ modulesPath, ... }: {
+          imports = [ (modulesPath + "/virtualisation/qemu-vm.nix") ];
+          
+          virtualisation.graphics = true;
+          
+          services.greetd = {
+            enable = true;
+            settings.default_session = {
+              command = "${self.packages.x86_64-linux.yawn}/bin/yawn -cmd bash";
+              user = "greeter";
+            };
+          };
+          
+          users.users.testuser = {
+            isNormalUser = true;
+            initialPassword = "test";
+          };
+          
+          networking.hostName = "yawn";
+
+          system.stateVersion = "24.11";
+        })
+      ];
+    };
+
+    apps.x86_64-linux.test-vm = {
+      type = "app";
+      program = "${self.nixosConfigurations.test-vm.config.system.build.vm}/bin/run-yawn-vm";
+    };
   };
 }
