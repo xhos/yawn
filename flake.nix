a6075a4ef657f5d103711e26229b50b52e3a7f42
xhos <60789741+xhos@users.noreply.github.com>
2025-11-23 20:17:38 -0500

    update dev scripts

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    git-hooks.url = "github:cachix/git-hooks.nix";
-    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -16,6 +18,21 @@
         system: function nixpkgs.legacyPackages.${system}
       );
   in {
+    # core package
+    packages = forAllSystems (pkgs: {
+      yawn = pkgs.buildGoModule {
+        pname = "yawn";
+        version = "0.1.0";
+        src = ./.;
+        vendorHash = "sha256-RNbS40G+8rtwlSJgYLN1puTCytGfXdagQTEs6sIXwnM=";
+        ldflags = ["-s" "-w"];
+        subPackages = ["cmd/yawn"];
+      };
+
+      default = self.packages.${pkgs.system}.yawn;
+    });
+
+    # dev stuff
     checks = forAllSystems (pkgs: {
       pre-commit = git-hooks.lib.${pkgs.system}.run {
         src = ./.;
@@ -29,46 +46,40 @@
 
     devShells = forAllSystems (pkgs: {
       default = pkgs.mkShell {
-        packages = with pkgs; [ go ];
+        packages = with pkgs; [
+          go
+
+          (pkgs.writeShellScriptBin "stub" ''
+            go run ./cmd/greetd-stub "$@"
+          '')
+        ];
+        shellHook = self.checks.${pkgs.system}.pre-commit.shellHook;
       };
     });
 
-    packages = forAllSystems (pkgs: {
-      yawn = pkgs.buildGoModule {
-        pname = "yawn";
-        version = "0.1.0";
-        src = ./.;
-        vendorHash = "sha256-RNbS40G+8rtwlSJgYLN1puTCytGfXdagQTEs6sIXwnM=";
-        ldflags = [ "-s" "-w" ];
-        subPackages = [ "cmd/yawn" ];
-      };
-
-      default = self.packages.${pkgs.system}.yawn;
-    });
-
+    # test VM
     nixosConfigurations.test-vm = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ({ modulesPath, ... }: {
-          imports = [ (modulesPath + "/virtualisation/qemu-vm.nix") ];
-          
+        ({modulesPath, ...}: {
+          imports = [(modulesPath + "/virtualisation/qemu-vm.nix")];
+
           virtualisation.graphics = true;
-          
+
           services.greetd = {
             enable = true;
             settings.default_session = {
-              command = "${self.packages.x86_64-linux.yawn}/bin/yawn -cmd bash";
+              command = "${self.packages.x86_64-linux.yawn}/bin/yawn -cmd bash -preauth -user test";
               user = "greeter";
             };
           };
-          
-          users.users.testuser = {
+
+          users.users.test = {
             isNormalUser = true;
             initialPassword = "test";
           };
-          
-          networking.hostName = "yawn";
 
+          networking.hostName = "yawn";
           system.stateVersion = "24.11";
         })
       ];
